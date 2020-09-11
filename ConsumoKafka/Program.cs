e7b2e208cd83354561bcb54ef7e48738c8e78bce
Lucas Scheid <lucas.scheid@penske.com>
2020-09-11 01:02:53 -0300

    Incluido projeto de exemplo com o uso do pacote nuget Confluent.Kafka

---
+++ ConsumoKafka/Program.cs
@@ -0,0 +1,65 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace ConsumoKafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Consumindo mensagens com o Kafka");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            const string bootstrapServers = "localhost:9092";
+            const string nomeTopic = "payment-request-confluent";
+
+            Console.WriteLine($"BootstrapServers = {bootstrapServers}");
+            Console.WriteLine($"Topic = {nomeTopic}");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = $"{nomeTopic}-group-0",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+                consumer.Subscribe(nomeTopic);
+
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                        Console.WriteLine(
+                            $"Mensagem lida: {cr.Message.Value}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                    Console.WriteLine("Cancelando a execução do consumidor...");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exceção: {ex.GetType().FullName} | " + $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ EnvioKafka/Program.cs
@@ -0,0 +1,55 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace EnvioKafka
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Enviando mensagens com o Kafka");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            const string bootstrapServers = "localhost:9092";
+            const string nomeTopic = "payment-request-confluent";
+            string payload = $"Mensagem => {Guid.NewGuid()} Hora => {DateTime.Now}";
+
+            Console.WriteLine($"BootstrapServers = {bootstrapServers}");
+            Console.WriteLine($"Topic = {nomeTopic}");
+
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServers
+                };
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    var result = await producer.ProduceAsync(
+                        nomeTopic,
+                        new Message<Null, string>
+                        { Value = payload });
+
+                    Console.WriteLine(
+                        $"Mensagem: {payload} | " +
+                        $"Status: { result.Status.ToString()}");
+                }
+
+                Console.WriteLine("Concluído o envio de mensagens");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exceção: {ex.GetType().FullName} | " + $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+
+}
